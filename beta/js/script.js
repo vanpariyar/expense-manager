fe6c84fa9ee4840d67aaca481ee685b86e235ee7
Ronak Vanpariya <vanpariyar@gmail.com>
2020-05-30 12:14:48 +0530

    addded.

--- beta/js/script.js
+++ beta/js/script.js
@@ -1,3 +1,17 @@
+/*
+var addMoreBtn = document.getElementById('addMore');
+var amountDetails = document.getElementsByClassName('amount-details');
+var clonedDetails =  amountDetails[0].innerHTML
+var i =0 ;
+
+function cloneAmountInput() {
+
+    // or clone.id = ""; if the divs don't need an ID
+    amountDetails[0].insertAdjacentHTML(amountDetails[0], clonedDetails);
+}
+
+addMoreBtn.addEventListener("click", cloneAmountInput);
+*/
 function myFunction() {
     var x = document.getElementById("myTopnav");
     (x.className === "topnav") ? x.className += " responsive" : x.className = "topnav";   
